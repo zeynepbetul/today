6dcd0936ef01ea36387aa71065b7ef974e4338e0
zeynepbetul <z.beetulkaya@gmail.com>
2025-10-15 16:40:34 +0300

    Add Models directory and Reminder.swift file with Reminder struct and property declarations

---
+++ Today/Models/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by Zeynep Betül Kaya on 15.10.2025.
+//
+
+import Foundation
+
+struct Reminder {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplete: Bool = false
+}
