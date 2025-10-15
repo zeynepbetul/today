7820b2c216d8f850e41dbcaaf0daf5a107b7dc76
zeynepbetul <z.beetulkaya@gmail.com>
2025-10-15 20:42:10 +0300

    Create a new array containing only the reminder titles, and append the titles as items in the snapshot.

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
 
     var dataSource: DataSource?
     
@@ -29,6 +30,13 @@ class ReminderListViewController: UICollectionViewController {
             return collectionView.dequeueConfiguredReusableCell(
                 using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        var reminderTitles = [String]()
+                for reminder in Reminder.sampleData {
+                    reminderTitles.append(reminder.title)
+                }
+                snapshot.appendItems(reminderTitles)
     }
 
     private func listLayout() -> UICollectionViewCompositionalLayout {
