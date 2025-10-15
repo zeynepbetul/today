df675930eaa2510b2bbcfb67272884d7f457d3ca
zeynepbetul <z.beetulkaya@gmail.com>
2025-10-15 18:25:14 +0300

    Create a list section in the collection view using compositional layout.

--- Today/ReminderListViewController.swift
+++ Today/ReminderListViewController.swift
@@ -11,9 +11,16 @@ class ReminderListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
     }
 
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
 
 }
 
